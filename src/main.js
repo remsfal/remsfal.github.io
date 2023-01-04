923234059b91999583d25de3a1df12f2e2799568
Alexander Stanik <a.stanik@avm.de>
2023-01-04 16:57:21 +0100

    added primevue menu bar

--- src/main.js
+++ src/main.js
@@ -1,11 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config';
+import Menubar from 'primevue/menubar';
+import Button from 'primevue/button';
 
+import 'primevue/resources/themes/saga-green/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeflex/primeflex.min.css'
+import 'primeicons/primeicons.css';
 import './assets/main.css'
 
 const app = createApp(App)
 
+// Install Router for SPA
 app.use(router)
+// Make PrimeVue available throughout the project
+app.use(PrimeVue)
+// Add primefaces components
+app.component('Menubar', Menubar)
+app.component('Button', Button)
 
 app.mount('#app')
