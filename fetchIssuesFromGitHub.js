a9f5bff556dfebf95a2699d46bfeb0735a8ce93e
alice.kitchkin <alice.kitchkin@siemens-energy.com>
2024-12-10 15:56:38 +0100

    Created a json file to fetch issues from remsfal/remsfal.github.io and log in console

---
+++ fetchIssuesFromGitHub.js
@@ -0,0 +1,19 @@
+// Token für die Authentifizierung bei der GitHub API
+const token = 'TOKEN';
+
+// Funktion, die alle Issues eines bestimmten Repositories abfragt
+const requestReposIssues = (name) => {
+    fetch(`https://api.github.com/repos/${name}/issues`, // URL für die Issues des Repositories
+        {
+            headers: { // Header zur Authentifizierung und API-Version
+                'Accept': 'application/vnd.github.v3+json',
+                'Authorization': `token ${token}` // Nutzung des persönlichen Tokens
+            }
+        })
+        .then(response => response.json()) // Antwort in JSON umwandeln
+        .then(data => {
+            console.log(data) // Ausgabe der Daten im Konsolenfenster
+        })
+}
+
+requestReposIssues('remsfal/remsfal.github.io');
