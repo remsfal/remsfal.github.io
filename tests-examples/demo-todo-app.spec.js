06f2a029ec738d3b5aa761d011b27f4070056349
alice.kitchkin <alice.kitchkin@siemens-energy.com>
2024-12-20 15:10:07 +0100

    Refactor test imports in demo-todo-app.spec.js to use ES module syntax

--- tests-examples/demo-todo-app.spec.js
+++ tests-examples/demo-todo-app.spec.js
@@ -1,5 +1,7 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+//const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
+
 
 test.beforeEach(async ({ page }) => {
   await page.goto('https://demo.playwright.dev/todomvc');
