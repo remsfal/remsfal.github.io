4eb9fca009f85c1e7cb1803a66e0b1b101395af4
alice.kitchkin <alice.kitchkin@siemens-energy.com>
2024-12-20 15:19:36 +0100

    Set baseURL in Playwright config to use environment variable or default to localhost

--- playwright.config.cjs
+++ playwright.config.cjs
@@ -27,6 +27,7 @@ module.exports = defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
+    baseURL: process.env.BASE_URL || 'http://localhost:3000',  // Use environment variable
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
