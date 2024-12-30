c0b9cb0276378bc6bf7dc5f6631018983f17f58f
Alexander Stanik <astanik@users.noreply.github.com>
2024-12-30 14:54:03 +0100

    added coverage report (#76)

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import IstanbulPlugin from 'vite-plugin-istanbul';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    ...(process.env.USE_BABEL_PLUGIN_ISTANBUL ? [IstanbulPlugin({
+      include: 'src/*',
+      exclude: ['node_modules', 'test/'],
+      extension: [ '.js', '.ts','.tsx' ],
+    })] : [])
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
