8e49f1de7bac1f793d780ec6f3723c18a203a846
JavascriptFreak <aishwary081997@gmail.com>
2025-07-11 14:10:38 +0200

    Added the coverage issue changes

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,13 +8,20 @@ export default mergeConfig(
     test: {
       coverage: {
         reporter: ['lcov', 'text', 'json', 'html'],
+        exclude: [
+          'tailwind.config.cjs',
+          'postcss.config.cjs',
+          'package.json',
+          'package-lock.json',
+          'vitest.config.ts',
+        ],
       },
       environment: 'jsdom',
       globals: true,
       mockReset: true,
       restoreMocks: true,
       clearMocks: true,
-      exclude: [...configDefaults.exclude],
+      exclude: [...configDefaults.exclude], 
       root: fileURLToPath(new URL('./', import.meta.url)),
     },
   }),
