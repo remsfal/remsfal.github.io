d482707ef918b1393323274501ec00d62eae5f81
JavascriptFreak <aishwary081997@gmail.com>
2025-07-14 11:38:11 +0200

    Added new changes for the doc render issues

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: ' REMSFAL Dokumentation',
-  description: "REMSFAL Dokumentation",
+  description: 'REMSFAL Dokumentation',
   lang: 'de-DE',
   outDir: resolve(__dirname, '../../dist/docs'),
   base: '/docs',
@@ -12,10 +12,16 @@ export default defineConfig({
     logo: 'logo.png',
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-   
-      { text: 'Forschung', link: process.env.VITE_Forschung_URL  || 'https://remsfal.de/research', target: '_self'},
-        { text: 'Dokumentation', link: process.env.VITE_DOCS_URL || '/docs/', target: '_self' },
-        { text: 'Kostenlos testen', link: process.env.VITE_Kostenlos_testen_URL || 'https://remsfal.online'}
+      {
+        text: 'Forschung',
+        link: process.env.VITE_DOCS_URL || 'http://localhost:5173/research',
+        target: '_self',
+      },
+      { text: 'Dokumentation', link: process.env.VITE_DOCS_URL || 'http://localhost:5174/docs/', target: '_self' },
+      {
+        text: 'Kostenlos testen',
+        link: process.env.VITE_Kostenlos_testen_URL || 'https://remsfal.online',
+      },
     ],
 
     sidebar: [
@@ -23,30 +29,30 @@ export default defineConfig({
         text: 'Inhalte',
         items: [
           { text: 'Objekthierachie', link: '/objekthierachie' },
-          { text: 'Projektdokumentation', link: '/projektdokumentation' }
-        ]
-      }
+          { text: 'Projektdokumentation', link: '/projektdokumentation' },
+        ],
+      },
     ],
     footer: {
       message: 'Released under the MIT License',
-      copyright: 'Copyright © 2023 - present Prof. Dr.-Ing. Alexander Stanik'
+      copyright: 'Copyright © 2023 - present Prof. Dr.-Ing. Alexander Stanik',
     },
     lastUpdated: {
-    text: 'Zuletzt aktualisiert',
-    formatOptions: {
-    dateStyle: 'full',
-    timeStyle: 'short'
-    }
+      text: 'Zuletzt aktualisiert',
+      formatOptions: {
+        dateStyle: 'full',
+        timeStyle: 'short',
+      },
     },
     docFooter: {
       prev: 'Vorherige Seite',
-      next: 'Nächste Seite'
+      next: 'Nächste Seite',
     },
     search: {
-      provider: 'local'
+      provider: 'local',
     },
     outline: {
       label: 'Auf dieser Seite',
-    }
-  }
+    },
+  },
 })
